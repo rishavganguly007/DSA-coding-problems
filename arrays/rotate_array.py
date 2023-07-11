c141af43035730c6ea52cff246937aa2b785cc3a
Rishav Ganguly <79499308+rishavganguly007@users.noreply.github.com>
2023-07-11 23:41:20 +0530

    Create rotate_array.py

---
+++ arrays/rotate_array.py
@@ -0,0 +1,13 @@
+# code -> https://leetcode.com/problems/rotate-array/
+
+def solve(arr, n):
+    temp = arr[0]  # storing the first element of the array in a variable
+    for i in range(n - 1):
+        arr[i] = arr[i + 1]
+    arr[n - 1] = temp  # assign the value of the variable at the last index
+    for i in range(n):
+        print(arr[i], end=" ")
+
+n = 5
+arr = [1, 2, 3, 4, 5]
+solve(arr, n)
