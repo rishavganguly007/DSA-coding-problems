08041e6eb8e571dc8a5fd34f20bf2d7423f3915a
Rishav Ganguly <79499308+rishavganguly007@users.noreply.github.com>
2023-09-14 00:26:47 +0530

    Create row_with_max_1s.py

--- binary_search/problems/row_with_max_1s.py
+++ binary_search/problems/row_with_max_1s.py
@@ -1,1 +1,40 @@
+# code -> https://www.codingninjas.com/studio/problems/row-of-a-matrix-with-maximum-ones_982768
 
+def findLowerBoundOf1(arr,m):
+    low = 0
+    high = m - 1
+
+    while low <= high:
+        mid = ( low + high ) // 2
+        if arr[mid] != 1:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return low
+
+def findTotal1(arr, m):
+    k = findLowerBoundOf1(arr,m)
+    if k == m:
+        return 0
+    return m - k
+
+def rowWithMax1s(matrix: [[int]], n: int, m: int) -> int:
+    index = -1
+    k = 0
+
+    for i in range(n):
+        temp = findTotal1(matrix[i], m)
+        if temp > k:
+            k = temp
+            index = i
+    return index
+
+
+"""
+n = 3 m = 3
+1  1  1
+0  0  1
+0  0  0
+Sample Output 1 :
+0
+"""
