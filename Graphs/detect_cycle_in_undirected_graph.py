bb054eea6060965a66e5a498023e31dbc6a0a324
Rishav Ganguly <79499308+rishavganguly007@users.noreply.github.com>
2023-10-19 21:29:50 +0530

    Update detect_cycle_in_undirected_graph.py

--- Graphs/detect_cycle_in_undirected_graph.py
+++ Graphs/detect_cycle_in_undirected_graph.py
@@ -1,7 +1,7 @@
 # code -> https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
 # resource -> https://www.youtube.com/watch?v=BPlrALf1LDU&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=7
 
-def detect(self, src, adj, vis):
+def detect_By_BFS(self, src, adj, vis):
         vis[src] = True
         q = Queue()
         q.put([src, -1])
@@ -18,6 +18,25 @@ def detect(self, src, adj, vis):
                 else:
                     if parent != i:
                         return True
+        return False
+
+def detect_By_DFS(self, src, adj, vis, parent):
+        vis[src] = True
+        for i in adj[src]:
+            if not vis[i]:
+                if self.detect_By_DFS(i, adj, vis, src):
+                    return True
+            elif i != parent:
+                return True
+        return False
+
+def isCycle(self, V: int, adj: List[List[int]]) -> bool:
+		#Code here
+        vis = [False] * V
+        for i in range(V):
+            if not vis[i]:
+                if self.detect_By_DFS(i, adj, vis, -1) :
+                    return True
         return False
   
 if __name__ == '__main__':
