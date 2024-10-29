c9c04408a1f192d34aeb0935c0381e914f358416
Rishav Ganguly <79499308+rishavganguly007@users.noreply.github.com>
2024-10-29 23:34:07 +0530

    Update coin_change.py

--- DP/coin_change.py
+++ DP/coin_change.py
@@ -2,9 +2,29 @@
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [[0 for _ in range(amount + 1)] for _ in range(len(coins))]
+        for i in range(amount+1):
+            if i % coins[0] == 0:
+                dp[0][i] = int(i / coins[0])
+            else:
+                dp[0][i] =  10**9
+        
+        for ind in range(1,len(coins)):
+            for t in range(amount+1):
+                notTake = dp[ind-1][t]
+                take = 10**9
+                if coins[ind] <= t:
+                    take = 1 + dp[ind][t-coins[ind]]
+                
+                dp[ind][t] = min(take, notTake)
+        res = dp[len(coins) - 1][amount]
+        return res if res != 10**9 else -1
+
+
+    def coinChange2(self, coins: List[int], amount: int) -> int:
         dp = [[-1 for _ in range(amount + 1)] for _ in range(len(coins))]
 
-        
+
 
         def f(ind, val):
             if dp[ind][val] != -1:
