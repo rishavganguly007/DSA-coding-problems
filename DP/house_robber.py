d39140c13aeba6f8b839264661a262cb44c08484
Rishav Ganguly <79499308+rishavganguly007@users.noreply.github.com>
2024-10-31 00:10:54 +0530

    Update house_robber.py

--- DP/house_robber.py
+++ DP/house_robber.py
@@ -2,6 +2,17 @@
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        dp = [-1] * len(nums)
+        dp[0] = nums[0]
+
+        for i in range(1,len(nums)):
+            if i-2>= 0:
+                dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+            else:
+                dp[i] = max(dp[i-1], nums[i])
+        return dp[len(nums)-1]
+        
+    def rob2(self, nums: List[int]) -> int:
         if len(nums) <= 2:
             return max(nums)
         dp = [-1] * len(nums)
