c3adc7e84a8c2a6769914870037e000632628762
Rishav Ganguly <79499308+rishavganguly007@users.noreply.github.com>
2024-11-11 23:44:51 +0530

    Create prime_substraction_operation.py

---
+++ binary_search/problems/prime_substraction_operation.py
@@ -0,0 +1,20 @@
+#code: https://leetcode.com/problems/prime-subtraction-operation/description/
+
+valid = [True] * 1001
+valid[0] = valid[1] = False
+for i in range(2, len(valid)):
+    if valid[i]:
+        for j in range(i * i, len(valid), i):
+            valid[j] = False
+primes = [i for i in range(len(valid)) if valid[i]]
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        prev = 0
+        for num in nums:
+            if num <= prev:
+                return False
+            i = bisect_left(primes, num - prev) - 1
+            if i != -1:
+                num -= primes[i]
+            prev = num
+        return True
